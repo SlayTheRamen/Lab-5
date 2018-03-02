0cf095325a239036a80ce8ce6c50ffc08318201f
Jack Yang <jackguangyang@gmail.com>
2018-03-01 16:22:53 -0800

    job 6

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -27,6 +27,9 @@ class Views extends Application
             $converted[] = (array) $task;
         // and then pass them on
         $parms = ['display_tasks' => $converted];
+        // INSERT the next two lines
+        $role = $this->session->userdata('userrole');
+        $parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
         return $this->parser->parse('by_priority', $parms, true);
     }
 
@@ -36,10 +39,23 @@ class Views extends Application
         return $this->parser->parse('by_category', $parms, true);
     }
 
+// complete flagged items
+    function complete() {
+        $role = $this->session->userdata('userrole');
+        if ($role != ROLE_OWNER) redirect('/work');
 
-
-
-
+        // loop over the post fields, looking for flagged tasks
+        foreach($this->input->post() as $key=>$value) {
+            if (substr($key,0,4) == 'task') {
+                // find the associated task
+                $taskid = substr($key,4);
+                $task = $this->tasks->get($taskid);
+                $task->status = 2; // complete
+                $this->tasks->update($task);
+            }
+        }
+        $this->index();
+    }
 
 }
 
